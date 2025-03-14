9970f66abf76352b1c99b5cfc64204f1050589bb
Diego <diego.almeida@gmail.com>
2025-03-13 22:47:00 -0300

    arrumando codigo

--- src/apis/rotas.ts
+++ src/apis/rotas.ts
@@ -2,8 +2,12 @@ import { Router, Request, Response } from "express";
 import { sendEmails } from "../service/sendemails";
 const rotas = Router();
 // Get all books
+//TODO CONSTRUIR UM REDIRECIONADOR
 rotas.post("/sendemails", (req: Request, res: Response) => {
     res.json(sendEmails(req.body));
 });
+
+
+
 
 export default rotas ;
--- src/service/sendemails.ts
+++ src/service/sendemails.ts
@@ -2,5 +2,5 @@ import { Usuario } from "../types/usuario";
 import { sendPost } from "./sendRequest";
 
 export const sendEmails  =  async (usuarios : Usuario[])=>{
-    console.log(await sendPost(usuarios));
+    await sendPost(usuarios);
 }
