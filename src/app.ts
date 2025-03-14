0ec43ead3278f638cf20de07a36dea92e894977d
Anderson Fernandes <fernandesanderson14@gmail.com>
2025-03-13 23:40:14 -0300

    Add cors
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
-import express from 'express';
-import rotas from './apis/rotas';
+import cors from "cors";
+import express from "express";
+import rotas from "./apis/rotas";
 import bodyParser from "body-parser";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api", rotas);
 app.listen(PORT, () => {
   return console.log(`Express is listening at http://localhost:${PORT}`);
